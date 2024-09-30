6bd0f74656bbfca7a130b7061e2fd4129956ff72
HARINIPORSELVI <harinibalamurugan19@gmail.com>
2024-09-30 15:40:17 +0530

    Create size of int,doub,float,char.c

---
+++ size of int,doub,float,char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int a;
+float b;
+char c;
+double d;
+printf("size of int:%ld\n",sizeof(a));
+printf("size of float:%ld\n",sizeof(b));
+printf("size of char:%ld\n",sizeof(c));
+printf("size of double:%ld\n",sizeof(d));
+}
